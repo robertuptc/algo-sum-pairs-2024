57e3bd94bcc05ab11a380c7f45527d98006f0fed
robertuptc <robertuptc@gmail.com>
2024-06-06 11:35:24 -0400

    python problem finished

--- python/sum_pairs.py
+++ python/sum_pairs.py
@@ -1,1 +1,12 @@
-def sum_pairs():
+def sum_pairs(int_arr, desired_num):
+    answer = []
+
+    for i in range(0, len(int_arr) - 1):
+        for j in range((i + 1), len(int_arr)):
+            if int_arr[i] +  int_arr[j] == desired_num:
+                answer.append([int_arr[i], int_arr[j]])
+    
+    if len(answer):
+        return answer
+    else:
+        return 'unable to find pairs'
--- python/sum_pairs_spec.py
+++ python/sum_pairs_spec.py
@@ -1,3 +1,6 @@
 from sum_pairs import sum_pairs
 
 # write your specs here!
+sum_pairs([1,2,3,4,5], 9) # [[4,5]]
+sum_pairs([1,2,3,4,5], 7) # [[2,5], [3,4]]
+sum_pairs([3, 1, 5, 8, 2], 27) # 'unable to find pairs'
