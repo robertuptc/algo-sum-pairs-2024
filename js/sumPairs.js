c595de807a2a12f887758fd0bac3f72fdec3ba75
robertuptc <robertuptc@gmail.com>
2024-06-06 10:51:43 -0400

    js problem finished

--- js/sumPairs.js
+++ js/sumPairs.js
@@ -1,3 +1,12 @@
-exports.sumPairs = function() {
-
+exports.sumPairs = function(intArr, desiredNum) {
+    let answer = []
+    
+    for (let i = 0; i < intArr.length; i++) {
+        for (let j = i + 1; j < intArr.length; j++) {
+            if (intArr[i] + intArr[j] == desiredNum) {
+                answer.push([intArr[i], intArr[j]])
+            }
+        }
+    }
+    return !answer.length ? answer : 'unable to find pairs'
 };
